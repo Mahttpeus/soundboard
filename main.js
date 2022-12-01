8e227d7abe5a5f211308a4901b885f31345b1508
Matheus M Barbosa <73943781+Mahttpeus@users.noreply.github.com>
2022-12-01 12:05:32 -0300

    adicionando os arquivos do projeto

---
+++ main.js
@@ -0,0 +1,35 @@
+﻿function tocaEfeito(seletorEfeito) {
+    const elemento = document.querySelector(seletorEfeito);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+}
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const efeito = tecla.classList[1];
+    const idEfeito = `#efeito_${efeito}`
+
+    tecla.onclick = function () {
+        tocaEfeito(idEfeito);
+    }
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === 'Enter' || evento.code === 'Space') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+
+}
